5055c5ecb44d4d351d057df61e438d5e87e7d2e8
Tacus <184624026@qq.com>
2014-06-16 22:03:32 +0800

    modify

--- proj.android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/AndroidManifest.xml : \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/AndroidManifest.xml \
--- proj.android/bin/classes.dex.d
+++
@@ -1,9 +0,0 @@
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes.dex : \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/com/gannicus/BuildConfig.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/com/gannicus/R$attr.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/com/gannicus/R$drawable.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/com/gannicus/R$string.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/com/gannicus/R.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/classes/org/cocos2dx/cpp/AppActivity.class \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/dexedLibs/classes-9aa714bcc9afcd6eecfcbff7c7e7033a.jar \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/dexedLibs/annotations-2f479b3d7f8b85d0b9555d1fc1ef084e.jar \
