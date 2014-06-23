82ff1ca316411bacb956b09284b927acef040d74
gannicus <iam.moxuan@gmail.com>
2014-06-23 23:11:13 +0800

    sync files

--- proj.android/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/gen/com/gannicus/R.java \
- : /Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/res/drawable-hdpi/icon.png \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/res/drawable-ldpi/icon.png \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/res/drawable-mdpi/icon.png \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/res/values/strings.xml \
-/Users/zhangguobing/develop/cocos3.0/MyGame/proj.android/bin/AndroidManifest.xml \
